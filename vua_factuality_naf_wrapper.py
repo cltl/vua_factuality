6faa572a7aa010a32625b3f5b850bdf74994fee4
Antske <antske@gmail.com>
2015-07-16 22:39:41 +0200

    minor test

--- vua_factuality_naf_wrapper.py
+++ vua_factuality_naf_wrapper.py
@@ -453,9 +453,9 @@ def main(argv=None):
         call(my_inst_call)
         #call machine learner
         ml_output = tmpdir + '/myoutput.tsv'
-        mytimbl_call = ['timbl', '-mO:I1,2,3,4', '-k3', '-i', 'timbl.factuality.model.wgt', '-t', 'scripts/tmp/features.tsv.renumbered.inst', '-o', 'scripts/tmp/myoutput.tsv', '>', 'timblout']
+        mytimbl_call = ['timbl', '-mO:I1,2,3,4', '-k3', '-i', 'timbl.factuality.model.wgt', '-t', 'scripts/tmp/features.tsv.renumbered.inst', '-o', 'scripts/tmp/myoutput.tsv']
         #mytimbl_call = ['timbl', '-mO:I1,2,3,4', '-k3', '-i', 'timbl.factuality.model.wgt', '-t', tmpdir + '/features.tsv.renumbered.inst', '-o',  ml_output, '>', 'tmp/timblout']
-        call(mytimbl_call)
+        call(mytimbl_call, stderr=tmpdir+'/timblerror')
         #add output from machine learning to NAF file to factDictTense, ontology set to 'both' as default for now
         factDict = add_factuality_info_from_output(ml_output, 'both', factDictTense)
         #update and output nafobj
